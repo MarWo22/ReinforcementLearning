24a692862476539f13f5c07babf7451dc2605071
MarWo22 <83605811+MarWo22@users.noreply.github.com>
2022-12-02 20:04:32 +0100

    started working on agent

---
+++ agent.py
@@ -0,0 +1,12 @@
+
+class Agent:
+    def __init__(self, k, epochs, type):
+        self.k = k
+        self.epochs = epochs
+        self.q_t = [0] * k
+    
+    def gaussian():
+        pass
+
+    def bernouli():
+        pass
